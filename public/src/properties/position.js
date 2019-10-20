07d37ec70023a71ddaf1eb304be3673b28e45823
CloseRange <39788976+CloseRange@users.noreply.github.com>
2019-10-20 18:15:52 -0500

    Updated Position to include offsets

--- public/src/properties/position.js
+++ public/src/properties/position.js
@@ -1,8 +1,8 @@
 
 class position {
-  constructor(parent, x, y) {
+  constructor(parent, x, y, xoff, yoff) {
     this.parent = parent;
-    this.x = x;
-    this.y = y;
+    this.x = x + (xoff==undefined ? 0 : xoff);
+    this.y = y + (yoff==undefined ? 0 : yoff);
   }
 }
